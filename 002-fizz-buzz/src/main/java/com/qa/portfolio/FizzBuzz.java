f7a858178ec044734fb6ea39ff53e1920e655b9a
tayyibah906 <tayyibah906@gmail.com>
2020-06-01 19:05:02 +0100

    in progress

--- 002-fizz-buzz/src/main/java/com/qa/portfolio/FizzBuzz.java
+++ 002-fizz-buzz/src/main/java/com/qa/portfolio/FizzBuzz.java
@@ -12,8 +12,22 @@ public class FizzBuzz {
 	 * value of the the number as a string</li>
 	 * </ul>
 	 * @param number
+	 * @return 
 	 * @return "fizz", "buzz", "fizzbuzz" or numeric value as a string
 	 */
-	public String fizzBuzz(int number) {
+	public void fizzBuzz(int number) {
+		for (int i = 1; i <= number; i++) 
+        {
+            if (((i % 3) == 0) && ((i % 5) == 0))
+                System.out.println("fizzbuzz");
+            else if ((i % 3) == 0)
+                System.out.println("fizz");
+            else if ((i % 5) == 0)
+                System.out.println("buzz");
+            else
+                System.out.println(i);
+        }
+
+		
 	}
 }
